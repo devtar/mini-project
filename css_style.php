11eac88c80e4ded4500bebfb618bf0f0b707f529
devtar <devanshawasthi000@gmail.com>
2015-11-28 16:52:30 +0530

    Create css_style.php

---
+++ css_style.php
@@ -0,0 +1,75 @@
+html {
+	margn:0;
+	padgin:0;
+}
+
+body {
+	width: 100%;
+	
+	
+		background-color: #A52A2A;}
+
+
+input[type='text'] {
+	padding:5px; border: 1px solid black;
+	background: red;
+	color: white;
+	width: 500px;
+	height: 70px;
+	-webkit-border-radius:25px;
+	
+	
+	
+	background: -webkit-linear-gradient(top,DarkRed ,DarkSalmon  );
+	
+	text-shadow: rgb(100,100,100) 3px 3px 5px;
+	color:#f5f5dc;
+	background-color: #eee;
+	border: solid #aaa 2px;
+}
+
+select {
+	padding:10px; font: bold 20px Tahoma;
+}
+
+.home-table-td {
+	font-size:17px;
+	color:#555;
+	font-weight:bold;
+}
+
+.home-submit {
+	margin-top: 10px;
+	padding:7px 15px;
+	border-radius: 5px;
+	background-color: #226;
+	color: #fff;
+	border: solid #88a 1px;
+	font-size: 14px;
+	font-weight: bold; border: 1px solid  black;
+	background: pink;
+	color: white;
+
+
+}
+.home-submit:hover {
+	cursor: pointer;
+	background-color: #fff;
+	color: #111;
+}
+
+.striped-table tr:nth-child(odd) {
+   background-color: #eee;
+
+	-webkit-border-radius:25px;
+	width: 500px;
+	height: 70px;
+	
+	background: -webkit-linear-gradient(top,OrangeRed   ,PeachPuff    );
+	-webkit-box-shadow: rgba(100,100,100,.6) 8px 8px 5px;
+	text-shadow: rgb(100,100,100) 3px 3px 5px;
+	
+}
+.striped-table td {
+   padding:5px;
+}
